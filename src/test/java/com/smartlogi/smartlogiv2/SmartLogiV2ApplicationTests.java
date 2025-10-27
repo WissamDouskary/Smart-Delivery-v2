c5db3200c6a8a03541f7adc98676becb60d1ed2e
WissamDouskary <douskary.wissam@gmail.com>
2025-10-27 09:44:04 +0100

    first commit

---
+++ src/test/java/com/smartlogi/smartlogiv2/SmartLogiV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartlogi.smartlogiv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartLogiV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
