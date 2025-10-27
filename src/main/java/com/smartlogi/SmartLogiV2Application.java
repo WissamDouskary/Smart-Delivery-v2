567bacba17e3a31ca5c807f4152cfb2da97af602
WissamDouskary <douskary.wissam@gmail.com>
2025-10-27 14:42:00 +0100

    SMAR-1 create database migration with liquibase and create tables

--- src/main/java/com/smartlogi/SmartLogiV2Application.java
+++ src/main/java/com/smartlogi/SmartLogiV2Application.java
@@ -1,13 +1,11 @@
-package com.smartlogi.smartlogiv2;
+package com.smartlogi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SmartLogiV2Application {
-
     public static void main(String[] args) {
         SpringApplication.run(SmartLogiV2Application.class, args);
     }
-
 }
