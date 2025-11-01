223ac011dfca77cd6cccf90e8152560e2b5840b3
WissamDouskary <douskary.wissam@gmail.com>
2025-11-01 15:32:56 +0100

    fix using colisRepository in controller directly and use colisService in controller

--- src/main/java/com/smartlogi/controller/StatistiquesController.java
+++ src/main/java/com/smartlogi/controller/StatistiquesController.java
@@ -3,6 +3,7 @@ package com.smartlogi.controller;
 import com.smartlogi.dto.ApiResponse;
 import com.smartlogi.dto.responseDTO.LivraisonStatsDTO;
 import com.smartlogi.repository.ColisRepository;
+import com.smartlogi.service.ColisService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,15 +13,15 @@ import java.util.List;
 @RestController
 public class StatistiquesController {
 
-    private final ColisRepository colisRepository;
+    private final ColisService colisService;
 
-    public StatistiquesController(ColisRepository colisRepository) {
-        this.colisRepository = colisRepository;
+    public StatistiquesController(ColisService colisService) {
+        this.colisService = colisService;
     }
 
     @GetMapping("/api/statistiques/livreurs-zones")
     public ResponseEntity<ApiResponse<List<LivraisonStatsDTO>>> getStatsParLivreurEtZone() {
-        List<LivraisonStatsDTO> statsDTOList = colisRepository.getLivraisonStatsParLivreurEtZone();
+        List<LivraisonStatsDTO> statsDTOList = colisService.getLivraisonStatsParLivreurEtZone();
 
         ApiResponse<List<LivraisonStatsDTO>> apiResponse = new ApiResponse<>("get Stats avec succes", statsDTOList);
 
