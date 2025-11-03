81884191d4df61e15563dfb5b7cc40295bbbc7f7
WissamDouskary <douskary.wissam@gmail.com>
2025-11-03 15:09:12 +0100

    add dotenv dependecy to make spring-boot use .env file

--- src/main/java/com/smartlogi/SmartLogiV2Application.java
+++ src/main/java/com/smartlogi/SmartLogiV2Application.java
@@ -1,11 +1,15 @@
 package com.smartlogi;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SmartLogiV2Application {
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("SMTP-USERNAME", dotenv.get("SMTP-USERNAME"));
+        System.setProperty("SMTP-PASSWORD", dotenv.get("SMTP-PASSWORD"));
         SpringApplication.run(SmartLogiV2Application.class, args);
     }
 }
