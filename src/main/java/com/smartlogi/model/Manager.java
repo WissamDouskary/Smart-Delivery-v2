4fc01a3c6e0a29f9c6f25c4c46fcc892c2e01cb2
WissamDouskary <douskary.wissam@gmail.com>
2025-11-17 11:39:27 +0100

    create manager entity

---
+++ src/main/java/com/smartlogi/model/Manager.java
@@ -0,0 +1,65 @@
+package com.smartlogi.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "managers")
+public class Manager {
+    @Id
+    @Size(max = 36)
+    @Column(name = "id", nullable = false, length = 36)
+
+    private String id;
+
+    @Size(max = 225)
+    @NotNull
+    @Column(name = "nom", nullable = false, length = 225)
+    private String nom;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
