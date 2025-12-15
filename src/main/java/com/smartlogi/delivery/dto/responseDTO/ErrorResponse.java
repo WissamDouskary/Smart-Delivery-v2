6ddbf96f3be6bf3cfe43b5fb5efa9be817f7cf72
WissamDouskary <douskary.wissam@gmail.com>
2025-12-15 19:27:14 +0100

    feat: update ErrorResponse class for return response - update exception to handled by ErrorResponse and create an exception handler for AuthorizationDeniedException

--- src/main/java/com/smartlogi/delivery/dto/responseDTO/ErrorResponse.java
+++ src/main/java/com/smartlogi/delivery/dto/responseDTO/ErrorResponse.java
@@ -1,40 +1,29 @@
 package com.smartlogi.delivery.dto.responseDTO;
 
+import java.time.LocalDateTime;
 
 public class ErrorResponse {
     private int status;
     private String message;
     private String error;
-
-    public ErrorResponse() {}
+    private LocalDateTime timestamp;
 
     public ErrorResponse(int status, String message, String error) {
         this.status = status;
         this.message = message;
         this.error = error;
+        this.timestamp = LocalDateTime.now();
     }
 
-    public int getStatus() {
-        return status;
-    }
+    public int getStatus() { return status; }
+    public void setStatus(int status) { this.status = status; }
 
-    public void setStatus(int status) {
-        this.status = status;
-    }
+    public String getMessage() { return message; }
+    public void setMessage(String message) { this.message = message; }
 
-    public String getMessage() {
-        return message;
-    }
+    public String getError() { return error; }
+    public void setError(String error) { this.error = error; }
 
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public String getError() {
-        return error;
-    }
-
-    public void setError(String error) {
-        this.error = error;
-    }
+    public LocalDateTime getTimestamp() { return timestamp; }
+    public void setTimestamp(LocalDateTime timestamp) { this.timestamp = timestamp; }
 }
